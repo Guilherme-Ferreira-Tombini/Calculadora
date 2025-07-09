abe953ddeb3b4495193728b04fce0b07a5ab5126
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2025-07-09 10:31:57 -0300

    feat: inicial basic code

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -6,7 +6,38 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
+            float conta = 0;
+            float valor1, valor2;
             Console.WriteLine("=== Calculadora ===");
+            Console.Write(" 1 - Somar.\n 2 - Subtrair. \n 3 - Dividir. \n 4 - Multiplicar.");
+            int opcao = int.Parse(Console.ReadLine());
+            switch (opcao)
+            {
+                case 1:
+                    Numeros(out valor1, out valor2);
+                    conta = valor1 + valor2;
+                    break;
+                case 2:
+                    Numeros(out valor1, out valor2);
+                    conta = valor1 - valor2;
+                    break;
+                case 3:
+                    Numeros(out valor1, out valor2);
+                    conta = valor1 / valor2;
+                    break;
+                case 4:
+                    Numeros(out valor1, out valor2);
+                    conta = valor1 * valor2;
+                    break;
+            }
+            Console.WriteLine($"Resultado: {conta}");
         }
+         static void Numeros(out float valor1, out float valor2)
+            {
+                    Console.Write("Digite o primeiro numero: ");
+                    valor1 = float.Parse(Console.ReadLine());
+                    Console.Write("Digite o segundo numero: ");
+                    valor2 = float.Parse(Console.ReadLine());
+            }
     }
 }
